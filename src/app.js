59639f86132d4818b8b2aaec4ae6fe5e7aad1f76
Daniel <danielghannadi1206@gmail.com>
2026-02-25 22:10:41 +0330

    feat: Initialize express server and folder structure

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.use('/', (req, res) => {
+	res.status(200).json({ message: 'Welcome to the Application Manager'});
+});
+
+
+
+
+app.listen(PORT, () => {
+	console.log('Server is running on port ' + PORT);
+});
