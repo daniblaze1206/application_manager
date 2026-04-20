11d5cac0aefa04f5d42064487aa555fe28f1c09f
Daniel <danielghannadi1206@gmail.com>
2026-04-20 14:10:28 +0330

    feat: add application management module

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ require('./src/configs/db');
 const PORT = process.env.PORT || 5000;
 const app = express();
 const authRouter = require('./src/routes/authRouter');
+const appRouter = require('./src/routes/appRouter');
 const authMiddleware = require('./src/middlewares/auth');
 app.use(express.json());
 app.use(express.urlencoded());
@@ -14,7 +15,7 @@ app.use(cors({
 }));
 
 app.use('/api/auth', authRouter);
-
+app.use('/api/application', authMiddleware, appRouter);
 
 
 app.listen(PORT, () => {
